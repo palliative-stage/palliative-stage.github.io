f07c4289281963bb12c1f24fe5b159cfb65a1ecd
Talmon Friedlander <tfriedlander@accruent.com>
2021-12-27 18:46:13 +0200

    Fix configuration for github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Palliative Care',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://palliative-stage.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'palliative-stage', 
+  projectName: 'palliative-stage.github.io', 
+  deploymentBranch: 'master', 
+  trailingSlash: true, 
 
   presets: [
     [
