4e4e51031a4044048ba9aa45231c669dc02dfffd
Rafael Horvat <rafael.horvat@glowingblue.com>
2021-12-29 15:36:51 +0100

    Removed tagline, not used

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,6 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
 	title: 'Palliative Care',
-	tagline: 'Dinosaurs are cool',
 	url: 'https://palliative-stage.github.io',
 	baseUrl: '/',
 	onBrokenLinks: 'throw',
