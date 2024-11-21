f69e134e7fb2fc1f7daa4db3ffe6cfad9fa97a8e
Talmon Friedlander <talmonf@gmail.com>
2024-11-21 20:55:02 +0200

    revert_icon

--- src/css/pages/_app.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { inject } from '@vercel/analytics';
-
-inject();
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
