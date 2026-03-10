4f4bcf0febbb4617b32bd87e3cded716324df395
Talmon Friedlander <talmonf@gmail.com>
2026-03-10 23:12:44 +0200

    Analytics_fix2

--- api/analytics.js
+++ api/analytics.js
@@ -112,7 +112,7 @@ async function processEvent(client, event, meta) {
       extra, entry_id, country, device_type, browser_name, os_name, language,
       referrer_domain, utm_source, utm_medium, utm_campaign
     ) VALUES (
-      $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23
+      $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24
     )`,
     [
       event_id,
